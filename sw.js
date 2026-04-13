a39233107cecab68ff6d1e505ee4f6d885527a0a
ConradGray <info@conradgray.com>
2026-04-13 08:44:23 +0300

    Bump cache to v2, add skipWaiting and clients.claim to force update on all devices

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
-const CACHE_NAME = 'expenses-v1';
+const CACHE_NAME = 'expenses-v2';
 const urlsToCache = ['/'];
-
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
-          caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-        );
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
-
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        ).then(() => clients.claim())
+    );
+});
 self.addEventListener('fetch', event => {
     event.respondWith(
-          caches.match(event.request).then(response => response || fetch(event.request))
-        );
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
